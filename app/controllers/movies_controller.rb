e7ea96af078c9ff0ef7eb6730b0b5cc1e5fdcd5f
NovA-x-X <jblainepennington@gmail.com>
2015-11-05 02:56:48 +0000

    Corrected gemfile

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,5 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -11,62 +7,27 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.pluck(:rating).uniq
-    @checked_boxes = Hash.new(true)
-    @all_ratings.each{|r| @checked_boxes[r] = true}
-
-    id = params[:id]
-
-    if id == "title_header"
-      @movies = Movie.all.order(:title)
-      @title_class = "hilite";
-      session[:id] = id
-      session.delete(:ratings_)
-    elsif id == "release_date_header"
-      @movies = Movie.all.order(:release_date)
-      @release_class = "hilite"
-      session[:id] = id
-      session.delete(:ratings_)
-    else
-      if params[:ratings_].present?
-        session.delete(:id)
-      end
-      if session[:id].present?
-        if session[:id] == "title_header"
-          @movies = Movie.all.order(:title)
-          @title_class = "hilite"
-        elsif session[:id] == "release_date_header"
-          @movies = Movie.all.order(:release_date)
-          @release_class = "hilite"
-        end
-        flash.keep
-        redirect_to :id => session[:id]
-      else
-        @movies = Movie.all
+    if(!params.has_key?(:sort) && !params.has_key?(:ratings))
+      if(session.has_key?(:sort) || session.has_key?(:ratings))
+        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
       end
     end
-
-    if params[:ratings_].present?
-      rs = params[:ratings_].keys
-      if rs.empty?
-        if session[:ratings_].present?
-          rs = session[:ratings_].keys
-          @checked_boxes = params[:ratings_]
-        end
-      else
-        @checked_boxes = params[:ratings_]
-      end
-      @movies = Movie.where(rating: rs)
-      session[:ratings_] = @checked_boxes
+    @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
+    @all_ratings = Movie.all_ratings.keys
+    @ratings = params[:ratings]
+    if(@ratings != nil)
+      ratings = @ratings.keys
+      session[:ratings] = @ratings
     else
-      if session[:ratings_].present?
-        rs = session[:ratings_].keys
-        @checked_boxes = session[:ratings_]
-        @movies = Movie.where(rating: rs)
-        flash.keep
-        redirect_to :ratings_ => session[:ratings_]
+      if(!params.has_key?(:commit) && !params.has_key?(:sort))
+        ratings = Movie.all_ratings.keys
+        session[:ratings] = Movie.all_ratings
+      else
+        ratings = session[:ratings].keys
       end
     end
+    @movies = Movie.order(@sort).find_all_by_rating(ratings)
+    @mark  = ratings
 
   end
 
@@ -75,7 +36,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -86,7 +47,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -97,5 +58,20 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+
+  def similar_movie
+    id = params[:id]
+    @movie = Movie.find_by_id(id)
+    if (@movie.director.length == 0)
+      flash[:notice] = "'#{@movie.title}' has no director info."
+      redirect_to movies_path
+    end
+    @movies = Movie.find_all_by_director(@movie.director)
+  end
+  
+  def find_class(header)
+    params[:sort] == header ? 'hilite' : nil
+  end
+  helper_method :find_class
 
 end
