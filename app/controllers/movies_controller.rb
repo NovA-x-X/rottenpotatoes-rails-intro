d49e504daf977072c638073d89ae3ec06418ea5c
NovA-x-X <jblainepennington@gmail.com>
2015-11-05 03:00:12 +0000

    Corrected controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,28 +7,33 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if(!params.has_key?(:sort) && !params.has_key?(:ratings))
-      if(session.has_key?(:sort) || session.has_key?(:ratings))
-        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
-      end
-    end
-    @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
-    @all_ratings = Movie.all_ratings.keys
-    @ratings = params[:ratings]
-    if(@ratings != nil)
-      ratings = @ratings.keys
-      session[:ratings] = @ratings
-    else
-      if(!params.has_key?(:commit) && !params.has_key?(:sort))
-        ratings = Movie.all_ratings.keys
-        session[:ratings] = Movie.all_ratings
-      else
-        ratings = session[:ratings].keys
-      end
-    end
-    @movies = Movie.order(@sort).find_all_by_rating(ratings)
-    @mark  = ratings
+    session[:ratings] = params[:ratings] if params[:ratings]
+    session[:sort_order] = params[:sort_order] if params[:sort_order]
 
+    # redirect to RESTful path if session contains more info than provided in params
+    if (!params[:ratings] && session[:ratings]) || (!params[:sort_order] && session[:sort_order])
+      redirect_to movies_path(ratings: session[:ratings], sort_order: session[:sort_order])
+    end
+
+    query_base = Movie
+
+    if session[:ratings]
+      query_base = query_base.scoped(:conditions => { :rating => session[:ratings].keys })
+    end
+
+    if session[:sort_order]
+      query_base = query_base.scoped(:order => session[:sort_order])
+    end
+
+    @movies = query_base.all
+
+    @all_ratings = Movie.all_ratings
+
+    if session[:ratings]
+      @selected_ratings = session[:ratings]
+    else
+      @selected_ratings = {}
+    end
   end
 
   def new
@@ -58,20 +63,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
-  def similar_movie
-    id = params[:id]
-    @movie = Movie.find_by_id(id)
-    if (@movie.director.length == 0)
-      flash[:notice] = "'#{@movie.title}' has no director info."
-      redirect_to movies_path
-    end
-    @movies = Movie.find_all_by_director(@movie.director)
-  end
-  
-  def find_class(header)
-    params[:sort] == header ? 'hilite' : nil
-  end
-  helper_method :find_class
 
 end
