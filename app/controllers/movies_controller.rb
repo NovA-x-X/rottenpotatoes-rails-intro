cc4bc942d60acf4c70466cc66e48951efb633914
NovA-x-X <jblainepennington@gmail.com>
2015-11-05 03:48:48 +0000

    Corrected controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,76 +1,78 @@
 class MoviesController < ApplicationController
-  
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
 
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-    @all_ratings ||= [] # initializes rating array
-    Movie.all.order(:rating).each do |m| #orders movies by rating so the checkboxes will be in order
-      @all_ratings << m.rating unless @all_ratings.include? m.rating # adds all unique ratings to array
+    def initialize
+        @all_ratings = Movie.all_ratings
+        super
     end
-    if params[:sort_by].nil? && params[:ratings].nil? && session[:sort_by].nil? && session[:ratings].nil? # if the app was just opened
-      @movies = Movie.all # show all movies, unsorted
-    elsif !params[:sort_by].nil? || !params[:ratings].nil? # if a tab is clicked or the checkboxes are changed
-      session[:sort_by] = params[:sort_by] unless params[:sort_by].nil? # store the new tab is session unless it wasn't changed
-      session[:ratings] = params[:ratings] unless params[:ratings].nil? # store the checkboxes is session unless they weren't changed
-      if params[:sort_by].nil? && !params[:ratings].nil? # if no tab is clicked but checkboxes were changed
-        @movies = Movie.all.where(rating: session[:ratings].keys) # filter movies by rating
-      elsif !params[:sort_by].nil? && params[:ratings].nil? # if checkboxes weren't changed but a tab was clicked
-        @movies = Movie.all.order(session[:sort_by])
-        @movies = Movie.all.order(session[:sort_by]).where(rating: session[:ratings].keys) unless session[:ratings].nil? # sort movies by tab
-      elsif !params[:sort_by].nil? && !params[:ratings].nil? # if a tab is clicked and checkboxes are changed
-        @movies = Movie.all.order(session[:sort_by]).where(rating: session[:ratings].keys) # sort all of the movies and filter by rating
-      end
-    elsif params[:sort_by].nil? && params[:ratings].nil? && (!session[:sort_by].nil? || !session[:ratings].nil?) # if no changes are made but there were past changes
-      if session[:sort_by].nil? && !session[:ratings].nil? # if no tab has ever been clicked but checkboxes were changed
-        @movies = Movie.all.where(rating: session[:ratings].keys) # filter movies by rating
-        flash.keep
-        redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings]) #
-      elsif !session[:sort_by].nil? && session[:ratings].nil? # if checkboxes weren't ever changed but a tab was clicked
-        @movies = Movie.all.order(session[:sort_by]) # sort movies by tab
-        flash.keep
-        redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings])
-      elsif !session[:sort_by].nil? && !session[:ratings].nil? # if a tab was once clicked and checkboxes have been changed
-        @movies = Movie.all.order(session[:sort_by]).where(rating: session[:ratings].keys) # sort all of the movies and filter by rating
-        flash.keep
-        redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings])
-      end
+
+    def show
+        id = params[:id] # retrieve movie ID from URI route
+        @movie = Movie.find(id) # look up movie by unique ID
+        # will render app/views/movies/show.<extension> by default
     end
-  end
 
-  def new
-    # default: render 'new' template
-  end
+    def index
+        redirect = false
 
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
+        if params[:sort]
+            @sorting = params[:sort]
+        elsif session[:sort]
+            @sorting = session[:sort]
+            redirect = true
+        end
 
-  def edit
-    @movie = Movie.find params[:id]
-  end
+        if params[:ratings]
+            @ratings = params[:ratings]
+        elsif session[:ratings]
+            @ratings = session[:ratings]
+            redirect = true
+        else
+            @all_ratings.each do |rat|
+                (@ratings ||= { })[rat] = 1
+            end
+            redirect = true
+        end
 
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
+        if redirect
+            redirect_to movies_path(:sort => @sorting, :ratings => @ratings)
+        end
 
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
+        Movie.find(:all, :order => @sorting ? @sorting : :id).each do |mv|
+            if @ratings.keys.include? mv[:rating]
+                (@movies ||= [ ]) << mv
+            end
+        end
+
+        session[:sort]    = @sorting
+        session[:ratings] = @ratings
+    end
+
+    def new
+        # default: render 'new' template
+    end
+
+    def create
+        @movie = Movie.create!(params[:movie])
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
+
+    def edit
+        @movie = Movie.find params[:id]
+    end
+
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(params[:movie])
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+    end
 
 end
